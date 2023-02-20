9a14b3cb943a3f0f22926081b0a09a01045a7c77
Jay Wang <jay.wang@hp.com>
2023-02-20 16:40:56 +0800

    feat: use abc and env to decide implementation

--- index.py
+++ index.py
@@ -3,36 +3,40 @@ import requests
 import os
 from dependency_injector import containers, providers
 from dependency_injector.wiring import Provide, inject
+from abc import ABC, abstractmethod
 
-
-class ApiFetcher:
-    def __init__(self, url: str) -> None:
-        self.url = url
+class IApiFetcher(ABC):
+    @abstractmethod
+    def request(self):
+        pass
+class ApiFetcher(IApiFetcher):
     def request(self):
         result = requests.get("https://w3schools.com/python/demopage.htm")
-        return {
-            'text': result.text,
-            'url': self.url
-        }
-
+        return result.text
+class DifferentApiFetcher(IApiFetcher):
+    def request(self):
+        result = requests.get("https://www.google.com")
+        return result.text
 
 class Container(containers.DeclarativeContainer):
     config = providers.Configuration()
-    apiFetcher = providers.Singleton(ApiFetcher, config.env_url)
+    if config.event == 'w3schools':
+        apiFetcher = providers.Singleton(ApiFetcher)
+    else:
+        apiFetcher = providers.Singleton(DifferentApiFetcher)
 
 
 @inject
-def lambda_handler(event, context, apiFetcher: ApiFetcher = Provide[Container.apiFetcher]):
+def lambda_handler(event, context, apiFetcher: IApiFetcher = Provide[Container.apiFetcher]):
     # TODO implement
     result = apiFetcher.request()
     # print(result['url'])
     return {
         'statusCode': 200,
-        'body': result['text'],
-        'url': result['url']
+        'body': result,
     }
 
 
 container = Container()
-container.config.env_url.from_env('ENV_URL')
+container.config.event.from_env('ENV_EVENT')
 container.wire(modules=[__name__])
